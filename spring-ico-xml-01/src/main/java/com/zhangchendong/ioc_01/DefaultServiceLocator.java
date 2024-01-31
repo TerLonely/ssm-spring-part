6bcc07b4b0e0d49e54c98610aefc1a8e9f488f77
zhangchendong <847890638@qq.com>
2024-01-31 09:59:19 +0800

    1-30

---
+++ spring-ico-xml-01/src/main/java/com/zhangchendong/ioc_01/DefaultServiceLocator.java
@@ -0,0 +1,14 @@
+package com.zhangchendong;
+
+/**
+ * @author zhangchendong
+ * @data 2024/1/24 18:37
+ */
+public class DefaultServiceLocator {
+
+    private static FactoryService factoryService = new FactoryService();
+
+    public FactoryService createFactory(){
+        return factoryService;
+    }
+}
---
+++ spring-ico-xml-01/src/main/java/com/zhangchendong/ioc_01/FactoryService.java
@@ -0,0 +1,8 @@
+package com.zhangchendong;
+
+/**
+ * @author zhangchendong
+ * @data 2024/1/24 18:40
+ */
+public class FactoryService {
+}
